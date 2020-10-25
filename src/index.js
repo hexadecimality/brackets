5703207e4c760d9aeba37124d6057c13d5386640
Andrii Abramov <abramov.nd@gmail.com>
2020-10-25 22:26:14 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let modify = bracketsConfig.map((e) => e.join(''));
+    for (let i = 0; i < modify.length; i++) {
+        if (str.includes(modify[i])) {
+            str = str.replace(modify[i], '');
+            i = -1;
+        }
+    }
+    return (str) ? false : true;
 }
